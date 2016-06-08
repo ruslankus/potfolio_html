32df9166e91f5975277bd76837a830ab9d450849
Ruslan Kiricenko <ruslankus@yahoo.com>
2016-06-08 23:45:20 +0300

    last update

--- js/scripts.js
+++ js/scripts.js
@@ -56,9 +56,90 @@ $(function(){
             $('#top-menu').css('height', 'auto');
         }
         $("header .mask > div > h2").css("margin-top", screenHeight * koeff);
-
-
+    
     }
     
     
+    
+/* -------------------------------  popup section --------------------------- */
+
+    $(".project > a").click(function(){
+        console.log(this);
+
+        var spinner = createSpinner();
+
+        spinner.appendTo('body');
+
+        $("#loader").animate({opacity:1},500, function(){
+
+            var popup = $("<div />").attr('id','popup');
+            popup.load('partial.html',function () {
+                $('footer').after(this);
+                destoySpinner();
+
+                //registrium close
+                $(".top-cross > span, .button-holder > button").click(function () {
+
+                    $("#popup").animate({
+                        opacity: 0
+                    },1000,function () {
+                        $(this).remove();
+                    })
+
+                });
+
+            });//load
+
+        });
+
+        return false;
+    });
+    
+    $(".top-cross > span, .button-holder > button").click(function () {
+
+        destroyPopup();
+    });
+
+
+
+
+    function createSpinner(){
+
+        var spinnerHolder = $("<div />").attr('id','loader');
+        spinnerHolder.css('opacity',0);;
+        var row = $("<div />").addClass('row');
+        var cell = $("<div />").addClass('cell');
+
+        var img = $("<img>").attr('src','/images/gears.gif');
+
+        var loader = spinnerHolder.append(row.append(cell.append(img)));
+
+
+        console.log(loader);
+        return loader;
+
+    }//createspinner
+
+
+    function destoySpinner(){
+        $("#loader").animate({
+            opacity:0
+        },500, function(){
+            $(this).remove();
+        })
+    }
+
+    function destroyPopup(){
+
+        $("#popup").animate({
+            opacity: 0
+        },1000,function () {
+            $(this).remove();
+        })
+
+    }
+
+
+
+
 });
